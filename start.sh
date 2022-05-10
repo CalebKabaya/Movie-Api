07944acae0bd3dde6ab79397426e71e2a1a205aa
calebkabaya <calebkabaya@gmail.com>
2022-05-10 16:47:23 +0300

    added the ur

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export MOVIE_API_KEY=4f589c421458b81ef3026e201954e93d
-
+export DATABASE_URL='postgresql+psycopg2://moringa:Mbuguack@localhost/watchlist'
 export SECRET_KEY=1212121212
 export MAIL_USERNAME=calebtest2022@gmail.com
 export MAIL_PASSWORD=Mbuguack7
